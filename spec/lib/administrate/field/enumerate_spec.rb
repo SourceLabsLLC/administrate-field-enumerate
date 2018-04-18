9a03a86c048e708d46e1f1007a6f68d42ba4aca0
Jônatas Rancan de Souza <jonatasrancan@gmail.com>
2018-04-17 21:21:01 -0300

    Improve tests

--- spec/lib/administrate/field/enumerate_spec.rb
+++ spec/lib/administrate/field/enumerate_spec.rb
@@ -1,14 +1,54 @@
 require 'administrate/field/enumerate'
 
 describe Administrate::Field::Enumerate do
+  class CategoryType < EnumerateIt::Base
+    associate_values(
+      first_type:  0,
+      second_type: 1
+    )
+  end
+
+  class Dumb
+    extend EnumerateIt
+    attr_accessor :category_type
+
+    def initialize(category_type)
+      @category_type = category_type
+    end
+
+    has_enumeration_for :category_type, with: CategoryType
+  end
+
+  let(:page) { :show }
+  let(:data) { double(:data) }
+  let(:resource) { Dumb.new(1) }
+
+  let(:field) do
+    described_class.new(
+      :category_type,
+      data,
+      page,
+      resource: resource
+    )
+  end
+
   describe '#to_partial_path' do
     it 'returns a partial based on the page being rendered' do
-      page = :show
-      field = described_class.new(:status, 'status', page)
-
       path = field.to_partial_path
 
       expect(path).to eq("/fields/enumerate/#{page}")
+    end
+  end
+
+  describe '#choices' do
+    it 'returns a list containing the humanized name and the value for the enum' do
+      expect(field.choices).to eq([["First Type", 0], ["Second Type", 1]])
+    end
+  end
+
+  describe '#to_s' do
+    it 'returns a list containing the humanized name and the value for the enum' do
+      expect(field.to_s).to eq('Second Type')
     end
   end
 end
